0483273c6141fb6efdc31953799fc79e65e7745d
Alexandra Ostrenkova <aleksandra.ostrenkova@innowise-group.com>
2023-07-21 11:49:33 +0300

    #lesson 19 add routing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,29 @@ import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
 import Footer from "./components/footer/Footer";
 import Dialogs from "./components/dialog/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import News from "./components/news/News";
+import Music from "./components/music/Music";
+import Settings from "./components/settings/Settings";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
-                {/*<Profile />*/}
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path="/profile" element={<Profile />}/>
+                        <Route path="/dialog" element={<Dialogs />}/>
+                        <Route path="/news" element={<News />}/>
+                        <Route path="/music" element={<Music />}/>
+                        <Route path="/settings" element={<Settings />}/>
+                    </Routes>
+                </div>
+                <Footer/>
             </div>
-            <Footer/>
-        </div>
+        </BrowserRouter>
     );
 }
 
